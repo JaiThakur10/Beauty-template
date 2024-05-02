44f4897687553c20bf49fcc048ecc9cbc55f568c
JaiThakur10 <dev10jai@gmail.com>
2024-05-02 15:39:38 +0530

    some fixes

--- src/Nav/Nav.jsx
+++ src/Nav/Nav.jsx
@@ -8,18 +8,18 @@ function Nav() {
             <h1 className=' font-lexend font-doit text-xl flex items-center h-full '>beauteira.</h1>
         </div>
         <div className=' w-[600px] h-full  flex gap-24'>
-            <ul className=' text-[#545558] text-sm flex items-center gap-9'>
-                <li>About</li>
-                <li>Specialities</li>
-                <li>Price</li>
-                <li>Gallery</li>
-                <li>Contacts</li>
+            <ul className=' text-[#545558] text-sm flex items-center gap-9 '>
+                <li className='hover:text-black hover:cursor-pointer'>About</li>
+                <li className='hover:text-black hover:cursor-pointer'>Specialities</li>
+                <li className='hover:text-black hover:cursor-pointer'>Price</li>
+                <li className='hover:text-black hover:cursor-pointer'>Gallery</li>
+                <li className='hover:text-black hover:cursor-pointer'>Contacts</li>
 
             </ul>
 
             <div className='  h-[20px] my-auto flex  items-center w-[140px] group relative'>
-                <li className=' hidden group-hover:block  duration-500  text-[#938270] mb-1'> &#9679;</li>
-                <li className=' font-lexend font-smash text-xs text-[#938270] hover:translate-x-2 list-none hover:text-[#8F7D6B]  duration-700 '>ENROLL ONLINE</li>
+                <li className=' hidden group-hover:block  duration-500  text-[#BBA18E] mb-1'> &#9679;</li>
+                <li className=' font-lexend font-smash text-xs text-[#BBA18E] hover:translate-x-2 list-none hover:text-[#8F7D6B]  duration-700 hover:cursor-pointer '>ENROLL ONLINE</li>
             </div>
         </div>
       </div>
